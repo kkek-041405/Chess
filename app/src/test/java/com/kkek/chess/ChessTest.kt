5a4d3df1da3dd6c1d5790fd2b5828995a00aff24
Konatham komal eshwara kumar <komaleshwarakumarkonatham@gmail.com>
2024-11-16 16:46:02 +0530

    Add unit tests for new data structure and Piece class
    * Update `testInitialSetup` and `testReset` to use new `Piece` class
    * Add `testPlacePiece` and `testPlacePieceInvalidPosition` for `placePiece` method
    * Add `PieceTest` class with `testMove` and `testPieceType` methods

--- app/src/test/java/com/kkek/chess/ChessTest.kt
+++ app/src/test/java/com/kkek/chess/ChessTest.kt
@@ -16,13 +16,13 @@ class ChessTest {
     @Test
     fun testInitialSetup() {
         val expectedInitialSetup = mapOf(
-            "11" to Peice.Rook,
-            "81" to Peice.Rook,
-            "88" to Peice.Rook,
-            "18" to Peice.Rook
+            "11" to Piece(Piece.PieceType.ROOK, "11"),
+            "81" to Piece(Piece.PieceType.ROOK, "81"),
+            "88" to Piece(Piece.PieceType.ROOK, "88"),
+            "18" to Piece(Piece.PieceType.ROOK, "18")
         )
         for ((position, piece) in expectedInitialSetup) {
-            assertEquals(piece, chess.pos[position])
+            assertEquals(piece.type, chess.pos[position]?.type)
         }
     }
 
@@ -30,13 +30,42 @@ class ChessTest {
     fun testReset() {
         chess.reset()
         val expectedResetSetup = mapOf(
-            "11" to Peice.Rook,
-            "81" to Peice.Rook,
-            "88" to Peice.Rook,
-            "18" to Peice.Rook
+            "11" to Piece(Piece.PieceType.ROOK, "11"),
+            "81" to Piece(Piece.PieceType.ROOK, "81"),
+            "88" to Piece(Piece.PieceType.ROOK, "88"),
+            "18" to Piece(Piece.PieceType.ROOK, "18")
         )
         for ((position, piece) in expectedResetSetup) {
-            assertEquals(piece, chess.pos[position])
+            assertEquals(piece.type, chess.pos[position]?.type)
         }
+    }
+
+    @Test
+    fun testPlacePiece() {
+        val piece = Piece(Piece.PieceType.KING, "55")
+        chess.placePiece("55", piece)
+        assertEquals(piece.type, chess.pos["55"]?.type)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testPlacePieceInvalidPosition() {
+        val piece = Piece(Piece.PieceType.KING, "99")
+        chess.placePiece("99", piece)
+    }
+}
+
+class PieceTest {
+
+    @Test
+    fun testMove() {
+        val piece = Piece(Piece.PieceType.QUEEN, "22")
+        piece.move("33")
+        assertEquals("33", piece.position)
+    }
+
+    @Test
+    fun testPieceType() {
+        val piece = Piece(Piece.PieceType.BISHOP, "44")
+        assertEquals(Piece.PieceType.BISHOP, piece.type)
     }
 }
