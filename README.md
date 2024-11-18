89129edfba23b95e19eadff7eb8edbd6ecd3f008
Konatham komal eshwara kumar <komaleshwarakumarkonatham@gmail.com>
2024-11-18 11:00:30 +0530

    Add detailed instructions to README.md and more unit tests to ExampleUnitTest.kt
    * **README.md**
      - Add detailed instructions on how to set up Firebase
      - Add instructions on how to run the project
      - Add instructions on how to contribute
    
    * **ExampleUnitTest.kt**
      - Add unit tests to cover edge cases such as invalid moves, moving to occupied squares, out-of-bounds moves, moves to check, checkmate, and stalemate

--- README.md
+++ README.md
@@ -55,3 +55,69 @@ We welcome contributions to improve the project. To contribute, follow these ste
 
 If you encounter any issues or have feature requests, please open an issue on the GitHub repository. Provide as much detail as possible to help us understand and address the issue or request.
 
+## Firebase Setup Instructions
+
+To set up Firebase for this project, follow these steps:
+
+1. Go to the [Firebase Console](https://console.firebase.google.com/).
+
+2. Click on "Add project" and follow the instructions to create a new Firebase project.
+
+3. Once the project is created, click on "Add app" and select "Android".
+
+4. Register your app with the package name `com.KKEK.chess`.
+
+5. Download the `google-services.json` file and place it in the `app` directory of your project.
+
+6. In the Firebase Console, go to the "Authentication" section and enable "Anonymous" sign-in method.
+
+7. In the Firebase Console, go to the "Database" section and create a new Realtime Database. Set the database rules to allow read and write access for authenticated users.
+
+8. Sync your project with Gradle files to ensure that the Firebase dependencies are added.
+
+## Running the Project
+
+To run the project, follow these steps:
+
+1. Open the project in Android Studio.
+
+2. Connect an Android device or start an emulator.
+
+3. Click on the "Run" button in Android Studio to build and run the project.
+
+4. The app should launch on the connected device or emulator, and you should be able to play chess online or against the computer.
+
+## Contributing Guidelines
+
+We welcome contributions to improve the project. To contribute, follow these steps:
+
+1. Fork the repository.
+
+2. Create a new branch for your feature or bugfix:
+   ```bash
+   git checkout -b my-feature-branch
+   ```
+
+3. Make your changes and commit them:
+   ```bash
+   git commit -m "Add new feature"
+   ```
+
+4. Push your changes to your forked repository:
+   ```bash
+   git push origin my-feature-branch
+   ```
+
+5. Create a pull request to the main repository.
+
+6. Ensure that your code follows the project's coding style and conventions.
+
+7. Write unit tests to cover your changes and ensure the robustness of the code.
+
+8. Update the documentation if necessary.
+
+9. Be responsive to feedback and make any requested changes promptly.
+
+10. Once your pull request is approved, it will be merged into the main repository.
+
+Thank you for contributing to the project!
--- app/src/test/java/com/KKEK/chess/ExampleUnitTest.kt
+++ app/src/test/java/com/KKEK/chess/ExampleUnitTest.kt
@@ -100,4 +100,69 @@ class ExampleUnitTest {
         assertEquals(PieceType.ROOK, gameViewModel.board[0][0].piece.value.type)
         assertEquals(PieceType.ROOK, gameViewModel.board[7][0].piece.value.type)
     }
+
+    @Test
+    fun testInvalidMove() {
+        val gameViewModel = GameViewModel()
+        val startSquare = Square(1, 0, mutableStateOf(Piece(PieceType.PAWN, PieceColor.BLACK)))
+        val endSquare = Square(3, 0, mutableStateOf(Piece(PieceType.None, PieceColor.None)))
+        assertFalse(gameViewModel.isValidMove(startSquare, endSquare, PieceColor.BLACK))
+    }
+
+    @Test
+    fun testMovePieceToOccupiedSquare() {
+        val gameViewModel = GameViewModel()
+        val startSquare = Square(1, 0, mutableStateOf(Piece(PieceType.PAWN, PieceColor.BLACK)))
+        val endSquare = Square(2, 0, mutableStateOf(Piece(PieceType.PAWN, PieceColor.BLACK)))
+        assertFalse(gameViewModel.movePiece(startSquare, endSquare))
+    }
+
+    @Test
+    fun testMovePieceOutOfBounds() {
+        val gameViewModel = GameViewModel()
+        val startSquare = Square(1, 0, mutableStateOf(Piece(PieceType.PAWN, PieceColor.BLACK)))
+        val endSquare = Square(8, 0, mutableStateOf(Piece(PieceType.None, PieceColor.None)))
+        assertFalse(gameViewModel.isValidMove(startSquare, endSquare, PieceColor.BLACK))
+    }
+
+    @Test
+    fun testMovePieceToCheck() {
+        val gameViewModel = GameViewModel()
+        val kingSquare = gameViewModel.board[0][4]
+        kingSquare.piece.value = Piece(PieceType.KING, PieceColor.BLACK)
+        val attackingSquare = gameViewModel.board[1][4]
+        attackingSquare.piece.value = Piece(PieceType.ROOK, PieceColor.WHITE)
+        val startSquare = gameViewModel.board[0][3]
+        startSquare.piece.value = Piece(PieceType.QUEEN, PieceColor.BLACK)
+        val endSquare = gameViewModel.board[1][3]
+        assertFalse(gameViewModel.movePiece(startSquare, endSquare))
+    }
+
+    @Test
+    fun testMovePieceToCheckmate() {
+        val gameViewModel = GameViewModel()
+        val kingSquare = gameViewModel.board[0][4]
+        kingSquare.piece.value = Piece(PieceType.KING, PieceColor.BLACK)
+        val attackingSquare1 = gameViewModel.board[1][4]
+        attackingSquare1.piece.value = Piece(PieceType.ROOK, PieceColor.WHITE)
+        val attackingSquare2 = gameViewModel.board[1][5]
+        attackingSquare2.piece.value = Piece(PieceType.ROOK, PieceColor.WHITE)
+        val startSquare = gameViewModel.board[0][3]
+        startSquare.piece.value = Piece(PieceType.QUEEN, PieceColor.BLACK)
+        val endSquare = gameViewModel.board[1][3]
+        assertFalse(gameViewModel.movePiece(startSquare, endSquare))
+    }
+
+    @Test
+    fun testMovePieceToStalemate() {
+        val gameViewModel = GameViewModel()
+        val kingSquare = gameViewModel.board[0][4]
+        kingSquare.piece.value = Piece(PieceType.KING, PieceColor.BLACK)
+        val blockingSquare = gameViewModel.board[1][4]
+        blockingSquare.piece.value = Piece(PieceType.PAWN, PieceColor.BLACK)
+        val startSquare = gameViewModel.board[0][3]
+        startSquare.piece.value = Piece(PieceType.QUEEN, PieceColor.BLACK)
+        val endSquare = gameViewModel.board[1][3]
+        assertFalse(gameViewModel.movePiece(startSquare, endSquare))
+    }
 }
